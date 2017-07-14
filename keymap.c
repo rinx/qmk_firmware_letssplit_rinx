6730ef0c66bdcb373e14697112486e3bb294d79c
Rintaro Okamura <rintaro.okamura@gmail.com>
2017-07-14 23:33:07 +0900

    Revise CHANGE_LAYER keys and some keymappings

--- keymap.c
+++ keymap.c
@@ -14,17 +14,21 @@ extern keymap_config_t keymap_config;
 #define _NUMPAD 3
 #define _ADJUST 16
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  NUMPAD,
-  ADJUST,
-};
-
-// Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+
+#define NEXTWKSP ACTION_MODS_KEY(MOD_LCTL, KC_RGHT)
+#define PREVWKSP ACTION_MODS_KEY(MOD_LCTL, KC_LEFT)
+#define NEXTTAB ACTION_MODS_KEY(MOD_LGUI, KC_RCBR)
+#define PREVTAB ACTION_MODS_KEY(MOD_LGUI, KC_LCBR)
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER = LT(_LOWER, PREVWKSP),
+  RAISE = LT(_RAISE, NEXTWKSP),
+  NUMPAD = LT(_NUMPAD, NEXTTAB),
+  ADJUST = LT(_ADJUST, PREVTAB),
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -50,17 +54,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------. ,-----------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  | |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  | |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  | |  F6  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 | |  F12 |ISO ~ |ISO | |      |      |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 | |  F12 |ISO ~ |ISO | |      |      | Del  |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * |      |      |      |      |      |      | |      |      |      |      |      |      |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
+  _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, KC_DEL, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
@@ -68,17 +72,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------. ,-----------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  | |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  | |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  | |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 | |  F12 |ISO # |ISO / |      |      |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 | |  F12 |ISO # |ISO / |      |      | Del  |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * |      |      |      |      |      |      | |      |      |      |      |      |      |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
+  _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, KC_DEL, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
@@ -103,23 +107,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust
  * ,-----------------------------------------. ,-----------------------------------------.
- * |      | Reset|      |      |      |      | |      |      |      |      |      |  Del |
+ * |RGB on|RGBmod|      | MsUp |      |      | |      |      | MsUp |      |RGB v-|RGB v+|
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |      |      |      |      |      | |      |Macro1|Macro2|      |      |      |
- * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |      |RGB on|RGBmod|RGB h-|RGB h+|      | |      |RGB s-|RGB s+|RGB v-|RGB v+|      |
+ * |RGB h-|RGB h+|MsLeft|MsDown|MsRght|      | |      |MsLeft|MsDown|MsRght|RGB s-|RGB s+|
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * |      |      |      |      |      |      | |      |      |      |      |      |      |
+ * |------+------+------+------+------+------| |------+------+------+------+------+------|
+ * |      |      |      |      | Lclk | Rclk | | Lclk | Rclk |      |      |      |      |
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_ADJUST] =  KEYMAP( \
-  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
-  _______, _______, _______, _______, _______, _______, _______, M(0),    M(1),    _______, _______, _______, \
-  _______, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, _______, _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  RGB_TOG, RGB_MOD, _______, KC_MS_U, _______, _______, _______, _______, KC_MS_U, _______, RGB_VAD, RGB_VAI, \
+  RGB_HUD, RGB_HUI, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, RGB_SAD, RGB_SAI, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_BTN1, KC_BTN2, _______, _______, _______, _______ \
 )
 
 
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    [1] = ACTION_LAYER_TAP_TOGGLE(_LOWER),  // FN1 - Momentary Layer 1
+    [2] = ACTION_LAYER_TAP_TOGGLE(_RAISE),  // FN2 - Momentary Layer 2
+    [3] = ACTION_LAYER_TAP_TOGGLE(_NUMPAD), // FN3 - Momentary Layer 3
+    [4] = ACTION_LAYER_TAP_TOGGLE(_ADJUST)  // FN3 - Momentary Layer 4
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -151,7 +162,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case QWERTY:
       if (record->event.pressed) {
         persistent_default_layer_set(1UL<<_QWERTY);
-        rgblight_mode(0);
+        rgblight_mode(1);
       }
       return false;
       break;
