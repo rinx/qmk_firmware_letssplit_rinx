7a87605ff1fc76dda0adc77ca00dddd984f3002b
Rintaro Okamura <rintaro.okamura@gmail.com>
2017-07-23 14:44:43 +0900

    Revise paren keys

--- keymap.c
+++ keymap.c
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  | |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------| |------+------+------+------+------+------|
- * |Adjust|Lower | Alt  | GUI  | Bksp |Space | |  Esc | Enter|   [  |   ]  |Raise |Numpad|
+ * |Adjust|Lower | Alt  | GUI  | Bksp |Space | |  Esc | Enter|   (  |   )  |Raise |Numpad|
  * `-----------------------------------------' `-----------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  ADJUST,  LOWER,   KC_LALT, KC_LGUI, KC_BSPC, KC_SPC,  KC_ESC,  KC_ENT,  KC_LBRC, KC_RBRC, RAISE,   NUMPAD \
+  ADJUST,  LOWER,   KC_LALT, KC_LGUI, KC_SPC,  KC_BSPC, KC_ESC,  KC_ENT,  KC_LPRN, KC_RPRN, RAISE,   NUMPAD \
 ),
 
 /* Lower
